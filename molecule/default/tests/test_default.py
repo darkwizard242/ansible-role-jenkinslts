dbf911f3dc396817aedf390fef8972b73f6bdfaf
darkwizard242 <am900820@gmail.com>
2019-10-20 16:37:16 -0400

    modified tests to accomodate assertion testinfra tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -40,12 +40,6 @@ def test_jenkinslts_repofile_isfile(host):
       host.file('/etc/yum.repos.d/jenkins.repo').is_file
 
 
-def test_jenkinslts_repofile_stableurl(host):
-    assert host.file('/etc/apt/sources.list.d/jenkins.list').contains == \
-      'debian-stable' or \
-      host.file('/etc/yum.repos.d/jenkins.repo').contains == 'redhat'
-
-
 def test_java_package_installed(host):
     assert host.package("openjdk-8-jdk").is_installed or \
       host.package("java-1.8.0-openjdk-devel").is_installed
@@ -64,26 +58,19 @@ def test_jenkinslts_package_installed(host):
 
 
 def test_jenkinslts_binary_exists(host):
-    assert host.file('/usr/lib/jenkins').exists
+    assert host.file('/usr/share/jenkins/jenkins.war').exists
 
 
-def test_jenkinslts_binary_directory(host):
-    assert host.file('/usr/lib/jenkins').is_directory
+def test_jenkinslts_binary_directory_exists(host):
+    assert host.file('/usr/share/jenkins').exists
+
+
+def test_jenkinslts_binary_directory_check(host):
+    assert host.file('/usr/share/jenkins/jenkins.war').is_directory
 
 
 def test_jenkinslts_binary_whereis(host):
-    assert host.check_output('whereis jenkins') == '/usr/lib/jenkins'
-
-
-def test_jenkinslts_http_port(host):
-    assert host.file('/etc/default/jenkins').contains == 'HTTP_PORT=8080' or \
-      host.file('/etc/sysconfig/jenkins').contains == 'JENKINS_PORT="8080"'
-
-
-def test_jenkinslts_heapsize_max(host):
-    assert host.file('/etc/default/jenkins').contains == \
-      'JAVA_ARGS="-Xmx256m"' or host.file('/etc/sysconfig/jenkins').contains \
-      == 'JAVA_ARGS="-Xmx256m"'
+    assert host.check_output('whereis jenkins') == 'jenkins: /usr/share/jenkins'
 
 
 def test_jenkinslts_service_is_running(host):
@@ -96,10 +83,6 @@ def test_jenkinslts_service_is_enabled(host):
 
 def test_jenkinslts_process_is_running(host):
     assert host.process.get(user="jenkins", comm="java")
-
-
-def test_jenkinslts_url_is_reachable(host):
-    assert host.addr('http://localhost:8080').is_reachable
 
 
 def test_jenkinslts_passwordfile_exists(host):
