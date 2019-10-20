0b3a39a43cfb717216cfeb5e1710e61b7bcfd219
darkwizard242 <am900820@gmail.com>
2019-10-20 16:42:00 -0400

    E501 flake8 correction

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -70,7 +70,8 @@ def test_jenkinslts_binary_directory_check(host):
 
 
 def test_jenkinslts_binary_whereis(host):
-    assert host.check_output('whereis jenkins') == 'jenkins: /usr/share/jenkins'
+    assert host.check_output('whereis jenkins') == \
+    'jenkins: /usr/share/jenkins'
 
 
 def test_jenkinslts_service_is_running(host):
